39c6316824f44e1e6950446803f2ad736a9d151d
Rohit-gif6999 <158134560+Rohit-gif6999@users.noreply.github.com>
2026-01-28 06:43:44 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3)  // we are using node.js ,not browser
+
+// dataTypes
+
+let name = "hitesh" // string
+let age = 20 // number
+let isLoggedIn = true  // boolean
+
+
+// number => 2 to power 53
+// bigInt
+// string =>""
+// boolean => true/false
+// null =>standlone value
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
