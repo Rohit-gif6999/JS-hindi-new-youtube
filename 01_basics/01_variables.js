53195dc8c25f6c2b01272e35e8c4fef23132e8ed
Rohit-gif6999 <158134560+Rohit-gif6999@users.noreply.github.com>
2026-01-28 06:12:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "rohit@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 23   // not allowed we can not re declare const
+
+accountEmail = "rg@flipkart.com"
+accountPassword = "6789"
+accountCity = "Bengaluru"
+let accountState 
+
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
