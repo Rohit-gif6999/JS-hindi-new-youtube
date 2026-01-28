c24698dde0a6fb6f4909c9935477df538f1f99ff
Rohit-gif6999 <158134560+Rohit-gif6999@users.noreply.github.com>
2026-01-28 07:21:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,40 @@
+// let score = "33abc"   // NaN
+// let score = null     // 0
+// let score = undefined   // NaN
+// let score = true   // 1
+// let score = false   // 0
+
+let score = "Rohit"  // NaN
+
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
