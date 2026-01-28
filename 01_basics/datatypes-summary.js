dd745af144d4afb36b06a74b12b35aa217009864
Rohit-gif6999 <158134560+Rohit-gif6999@users.noreply.github.com>
2026-01-28 11:25:32 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive (call by value)
+
+// 7 types : string,number,Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isloggedin = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+const bigNumber = 45612236n
+
+
+
+
+
+// Non Primitive(call by reference)
+
+// arrays,functions,objects
+
+
+const heros = ["shaktiman", "naagraj","doga"] // arrays
+
+let myObj = {           // objects
+   name: "rohit",
+   age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof object)
