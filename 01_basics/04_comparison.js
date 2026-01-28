fe09eef175615c014a7b4bb06e2de192adbfa3d2
Rohit-gif6999 <158134560+Rohit-gif6999@users.noreply.github.com>
2026-01-28 08:36:45 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2===1)
+
+
+// console.log("2">0)             // true
+// console.log("02">0)            // true
+
+
+/*
+
+
+// console.log(null > 0)             // false
+// console.log(null == 0)            // false
+// console.log(null >= 0)            // true
+
+
+// console.log(undefined == 0)         // false
+// console.log(undefined < 0)         // false
+// console.log(undefined > 0)         // false
+
+avoid this
+*/
+
+// ===
+
+console.log("2"===2)
