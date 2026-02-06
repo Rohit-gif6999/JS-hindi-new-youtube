fdfa86ebbc3726fa507cbfaaadaad01440e8100c
Rohit-gif6999 <158134560+Rohit-gif6999@users.noreply.github.com>
2026-02-06 14:56:35 +0000

    Destructure and API, JSON

---
+++ 02_basics/05_objects_de-structure.js
@@ -0,0 +1,30 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+
+// course.courseInstructor
+
+const{courseInstructor} = course
+// console.log(courseInstructor);
+// const{courseInstructor:instructor} = course
+// console.log(courseInstructor);
+// console.log(instructor);
+
+
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
